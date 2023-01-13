3d87ade2fa82301b0999f1598d96de547cb9085c
Sickoko <Usukhuumz12@gmail.com>
2023-01-13 23:57:20 +0800

    color and size choice

--- react-ecommerce/src/components/detail/main.jsx
+++ react-ecommerce/src/components/detail/main.jsx
@@ -6,14 +6,25 @@ export default function Main() {
       <div className="left-side">
         <img src="images/neoncontroller.jpg" alt="" />
       </div>
-      <div className="right-side">
+      <div className="right-side text-start">
         <h2>Play game</h2>
         <p className="price">$11,70</p>
         <Rating />
         <p>Availability</p>
         <p>Hurry up! only 34 product left in stock!</p>
         <hr />
-        <p>Color:</p>
+        <p className="d-flex">
+          Color:
+          <p className="color-sec ms-3">.</p>
+          <p className="dark-color ms-3">.</p>
+        </p>
+        <p className="d-flex">
+          Size:
+          <a href="" className="ms-3 border border-2 px-3">30</a>
+          <a href="" className="ms-3 border border-2 px-3">30</a>
+          <a href="" className="ms-3 border border-2 px-3">30</a>
+          <a href="" className="ms-3 border border-2 px-3">30</a>
+        </p>
       </div>
     </div>
   );
