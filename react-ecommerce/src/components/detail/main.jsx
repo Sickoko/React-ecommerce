f6853c1d7de8969f7a676b6687696a43c0955ade
Sickoko <Usukhuumz12@gmail.com>
2023-01-14 23:27:32 +0800

    worked on it a little more

--- react-ecommerce/src/components/detail/main.jsx
+++ react-ecommerce/src/components/detail/main.jsx
@@ -2,37 +2,73 @@ import React from "react";
 import { Rating } from "react-simple-star-rating";
 export default function Main() {
   return (
-    <div className="detail-cont mt-5">
+    <div className="detail-cont mt-5 d-flex">
       <div className="left-side">
         <img src="images/neoncontroller.jpg" alt="" />
       </div>
-      <div className="right-side text-start">
-        <h2>Play game</h2>
-        <p className="price">$11,70</p>
-        <Rating />
-        <p className="d-flex"> <strong>Availability: </strong><p className="text-success">In Stock</p></p>
-        <p>Hurry up! only 34 product left in stock!</p>
-        <hr />
-        <p className="d-flex">
-          Color:
-          <p className="color-sec ms-3">.</p>
-          <p className="dark-color ms-3">.</p>
-        </p>
-        <p className="d-flex">
-          Size:
-          <a href="" className="ms-3 border border-2 px-3 text-decoration-none text-dark">
-            30
-          </a>
-          <a href="" className="ms-3 border border-2 px-3 text-decoration-none text-dark">
-            56
-          </a>
-          <a href="" className="ms-3 border border-2 px-3 text-decoration-none text-dark">
-            42
-          </a>
-          <a href="" className="ms-3 border border-2 px-3 text-decoration-none text-dark">
-            48
-          </a>
-        </p>
+      <div className="right-side text-start align-items-start ms-5">
+        <div>
+          <h2>Play game</h2>
+          <p className="price">$11,70</p>
+          <Rating />
+          <p className="d-flex">
+            <strong>Availability: </strong>
+            <p className="text-success">In Stock</p>
+          </p>
+          <p>Hurry up! only 34 product left in stock!</p>
+          <hr />
+          <p className="d-flex">
+            Color:
+            <a href="#" className="color-sec ms-3 text-decoration-none">
+              .
+            </a>
+            <a href="#" className="dark-color ms-3 text-decoration-none">
+              .
+            </a>
+          </p>
+          <p className="d-flex">
+            Size:
+            <a
+              href=""
+              className="ms-3 border border-2 px-3 text-decoration-none text-dark"
+            >
+              30
+            </a>
+            <a
+              href=""
+              className="ms-3 border border-2 px-3 text-decoration-none text-dark"
+            >
+              56
+            </a>
+            <a
+              href=""
+              className="ms-3 border border-2 px-3 text-decoration-none text-dark"
+            >
+              42
+            </a>
+            <a
+              href=""
+              className="ms-3 border border-2 px-3 text-decoration-none text-dark"
+            >
+              48
+            </a>
+          </p>
+          <div className="mt-4">
+            <a
+              href="#"
+              className="cart-btn text-decoration-none text-white rounded-pill px-3"
+            >
+              Add to cart
+            </a>
+            <a
+              href="#"
+              className="cart-btn text-decoration-none ms-3 text-white rounded-pill px-3"
+            >
+              Buy it now
+            </a>
+          </div>
+          <hr />g
+        </div>
       </div>
     </div>
   );
